4107352a0a196538c76e0d941500c047d2e136c0
ibrahim <ibrahimkrimi2@gmail.com>
2024-07-13 18:35:27 +0200

    finich Skill

--- skill/src/main/java/org/example/skill/SkillApplication.java
+++ skill/src/main/java/org/example/skill/SkillApplication.java
@@ -1,7 +1,11 @@
 package org.example.skill;
 
+import org.example.skill.entites.Skill;
+import org.example.skill.repositories.SkillRepositories;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SkillApplication {
@@ -10,5 +14,15 @@ public class SkillApplication {
         SpringApplication.run(SkillApplication.class, args);
     }
 
-    
+    @Bean
+    CommandLineRunner commandLineRunner(SkillRepositories skillRepositories){
+        return args -> {
+            Skill skill1 = new Skill(null, "Java");
+            Skill skill2 = new Skill(null, "Spring Boot");
+
+            skillRepositories.save(skill1);
+            skillRepositories.save(skill2);
+        };
+
+    }
 }
--- skill/src/main/java/org/example/skill/controllers/SkillRestController.java
+++ skill/src/main/java/org/example/skill/controllers/SkillRestController.java
@@ -3,14 +3,12 @@ package org.example.skill.controllers;
 import org.example.skill.dtos.SkillDto;
 import org.example.skill.services.ISkillService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController@RequestMapping("skill")
+@RestController
+@RequestMapping("/skill")
 public class SkillRestController {
 
     private final ISkillService skillService;
@@ -20,12 +18,12 @@ public class SkillRestController {
         this.skillService = skillService;
     }
 
-    @RequestMapping("/")
+    @GetMapping
     public ResponseEntity<List<SkillDto>> getAll(){
         return ResponseEntity.ok(skillService.findAll());
     }
 
-    @RequestMapping("/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<SkillDto> getById(@PathVariable Long id){
         return ResponseEntity.ok(skillService.findById(id));
     }
